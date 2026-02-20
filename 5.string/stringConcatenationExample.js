8fb02450fc78fc9268c4295d044e8c0744d3da19
pratikadhikari2062 <adhikaripratik522@gmail.com>
2026-02-20 19:33:21 +0545

    string example and syntax

--- 5.string/stringConcatenationExample.js
+++ 5.string/stringConcatenationExample.js
@@ -1,5 +1,20 @@
-let firstname = "pratik";
+//that is a "pratik Adhikari" this type join string its a space in between.
+
+/* let firstname = "pratik";
 let secondname = "Adhikari";
 
 let fullname = firstname + " " + secondname;
-console.log(fullname);
+console.log(fullname); */
+
+//that is a "PratikAdhikari" its types string we didn't have a space;
+/* let firstname = "pratik";
+let secondname = "Adhikari";
+
+let fullname = firstname + secondname;
+console.log(fullname); */
+
+//that is a comma sepret string using the += operator:
+let greeting = 'Pratik';
+greeting += ',prabin!';
+
+console.log(greeting);
